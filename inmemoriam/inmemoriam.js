4e80ea647b845e43178b96115918e681b55dea7e
Laurens Marnix Kalf <laurens@sense-os.nl>
2021-09-18 11:14:42 +0200

    Update inmemoriam.js

--- inmemoriam/inmemoriam.js
+++ inmemoriam/inmemoriam.js
@@ -1,49 +1,55 @@
-// <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-// <script type="text/javascript">
-/* An HTML/Javascript code snippet for Ecwid to show/hide product options depending on a trigger(expander) option */
-if (typeof(Ecwid) == 'object') {
-  Ecwid.OnAPILoaded.add(function() {
-
-    var expanderOptionName = "Do_you_want_extra_options";
-    var expanderOptionFlagValue = "Yes";
-
-    function checkExpandableOptions() {
-      // Detect the current selection of the 'expander' option
-      var expanderOptionCurrentValue = jQuery("div[class *= ecwid-productoption-" + expanderOptionName + "] input:checked + label > span").text();
-
-      // Show or hide product options depending on the 'expander' option value
-      if (expanderOptionCurrentValue == expanderOptionFlagValue) {
-        // Show all options
-        jQuery('div.ecwid-productBrowser-details-optionPanel').show();
-
-      } else {
-        // Hide all options
-        jQuery('div.ecwid-productBrowser-details-optionPanel').hide();
-
-        // Show expander option
-        jQuery("div[class *= ecwid-productoption-" + expanderOptionName + "]").show();        
-      }
-    }
-
-    Ecwid.OnPageLoaded.add(function(page) {
-      if ('PRODUCT' == page.type) {
-        checkExpandableOptions();
-       
-        // Attach a handler to the 'expander' option
-        jQuery("div[class *= ecwid-productoption-" + expanderOptionName + "] input[type=radio]").click(function() {
-          checkExpandableOptions();
-        });
-      }
-    });
-  });
-}
+console.log(document.getElementById('form-control__radio---340967026').value);
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log(document.getElementById('form-control__radio---340967026').value);
 });
 
-
 console.log("Hoi Fab, we kunnen aan de slag!");
+
+
+// <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+// <script type="text/javascript">
+/* An HTML/Javascript code snippet for Ecwid to show/hide product options depending on a trigger(expander) option */
+// if (typeof(Ecwid) == 'object') {
+//   Ecwid.OnAPILoaded.add(function() {
+
+//     var expanderOptionName = "Do_you_want_extra_options";
+//     var expanderOptionFlagValue = "Yes";
+
+//     function checkExpandableOptions() {
+//       // Detect the current selection of the 'expander' option
+//       var expanderOptionCurrentValue = jQuery("div[class *= ecwid-productoption-" + expanderOptionName + "] input:checked + label > span").text();
+
+//       // Show or hide product options depending on the 'expander' option value
+//       if (expanderOptionCurrentValue == expanderOptionFlagValue) {
+//         // Show all options
+//         jQuery('div.ecwid-productBrowser-details-optionPanel').show();
+
+//       } else {
+//         // Hide all options
+//         jQuery('div.ecwid-productBrowser-details-optionPanel').hide();
+
+//         // Show expander option
+//         jQuery("div[class *= ecwid-productoption-" + expanderOptionName + "]").show();        
+//       }
+//     }
+
+//     Ecwid.OnPageLoaded.add(function(page) {
+//       if ('PRODUCT' == page.type) {
+//         checkExpandableOptions();
+       
+//         // Attach a handler to the 'expander' option
+//         jQuery("div[class *= ecwid-productoption-" + expanderOptionName + "] input[type=radio]").click(function() {
+//           checkExpandableOptions();
+//         });
+//       }
+//     });
+//   });
+// }
+
+
+
+
 // </script>
 
 // function to load jQuery
