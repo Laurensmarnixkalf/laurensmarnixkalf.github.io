ed023c4793a2c489731681fb695ca92c37aa83ae
Laurens Marnix Kalf <laurens@sense-os.nl>
2021-09-18 11:18:49 +0200

    Update inmemoriam.js

--- inmemoriam/inmemoriam.js
+++ inmemoriam/inmemoriam.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log(document.getElementById('form-control__radio---340967026').value);
+  console.log(document.getElementById('form-control__radio---340967026').innerText);
 });
 
 console.log("Hoi Fab, we kunnen aan de slag!");
