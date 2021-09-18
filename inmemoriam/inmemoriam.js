48a3f09e485ccab990ca69379227505fe2260182
Laurens Marnix Kalf <laurens@sense-os.nl>
2021-09-18 13:06:14 +0200

    Update inmemoriam.js

--- inmemoriam/inmemoriam.js
+++ inmemoriam/inmemoriam.js
@@ -1,3 +1,8 @@
+ window.ec = window.ec || Object();
+  window.ec.storefront = window.ec.storefront || Object();
+  window.ec.storefront.product_details_show_product_name = FALSE; 
+  Ecwid.refreshConfig();
+
 Ecwid.OnAPILoaded.add(function() {
   console.log("Ecwid storefront JS API has loaded");
 
