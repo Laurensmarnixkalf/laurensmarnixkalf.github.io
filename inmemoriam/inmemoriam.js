1a4911fd87f7111c80ff2bafcf3f693c3c18c0c5
FerdinandNathaniel <fabiankok@protonmail.com>
2021-09-20 22:47:14 +0200

    debugging

--- inmemoriam/inmemoriam.js
+++ inmemoriam/inmemoriam.js
@@ -73,6 +73,8 @@ Ecwid.OnPageLoaded.add(function (page) {
 
 Ecwid.OnProductOptionsChanged.add(function () {
 
+  console.log("onproductoptionschanged has fired");
+
   function hide(classname) {
     //gegeven een classname, hide het element
     document.getElementsByClassName(classname)[0].style.display = 'none';
@@ -98,36 +100,31 @@ Ecwid.OnProductOptionsChanged.add(function () {
   // check of rouwkaartjes/lintje gewild is
   var extra = document.querySelector('[aria-label="Luxe kaartje / Rouwlint*"]');
 
+  console.log("extra value = " + extra.value);
+
   if (extra.value == "Geen") {
     console.log("Geen");
     hide(classname_luxekaartje);
     hide(classname_rouwlint_1);
     hide(classname_rouwlint_2);
     hide(classname_rouwlint_3);
-  }
-
-  if (extra.value == "Luxe kaartje") {
+  } else if (extra.value == "Luxe kaartje") {
     console.log("Rouwkaartje");
     show(classname_luxekaartje);
     hide(classname_rouwlint_1);
     hide(classname_rouwlint_2);
     hide(classname_rouwlint_3);
-  }
-
-  if (extra.value == "Luxe rouwlint [2 stuks]") {
+  } else if (extra.value == "Luxe rouwlint [2 stuks]") {
     hide(classname_luxekaartje);
     show(classname_rouwlint_1);
     show(classname_rouwlint_2);
     hide(classname_rouwlint_3);
-  }
-
-  if (extra.value == "Luxe rouwlint [3 stuks]") {
+  } else if (extra.value == "Luxe rouwlint [3 stuks]") {
     hide(classname_luxekaartje);
     show(classname_rouwlint_1);
     show(classname_rouwlint_2);
     show(classname_rouwlint_3);
   }
-
 });
 
 
